36c5d2151aa0053a0bbe31a8ba24ef4ad57ccae0
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-10-22 20:36:52 -0300

    teste3

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,7 +3,7 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
 
     const res = await fetch(url);
-    const dados = res.json();
+    const dados = await res.json();
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`;
